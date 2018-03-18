5547fd6ebfee155a36f23aee03e739691885ca56
agnewee <agnewee@gmail.com>
2018-03-18 17:26:01 +0800

    add download novel script

---
+++ crawler/dowload_novel.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+"""
+python 3.6
+小说只能在线观看，有点麻烦，下载到本地保存为txt文本
+下载www.biquge.cc网站的小说
+"""
+import sys
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+
+header = {
+         'Accept': 'text/html',
+         'Content-Type': 'text/html',
+         'User-Agent': 'Mozilla/5.0'
+         }
+
+
+def force_charset(response, *args, encoding='gbk', **kwargs):
+    response.encoding = encoding
+    return None
+
+
+def get_content(resource_url, http_header):
+    res = requests.get(resource_url,
+                       headers=http_header,
+                       hooks={'response': [force_charset]})
+    if res.status_code == 200:
+        return res.text
+    else:
+        return None
+
+
+def get_sections_url(soup, div_id):
+    urls = (url.get('href') for url in soup.find(id=div_id).find_all('a'))
+    return urls
+
+
+base_url = 'http://www.xbiquge.cc'
+# book_id = input('清输入小说书号: ')
+book_id = '24639'
+book_base_url = 'book/{}'.format(book_id)
+url = urljoin(base_url, book_base_url)
+html = get_content(url, header)
+if not html:
+    print('获取书籍页面失败，请确认')
+    print('退出下载')
+    sys.exit(1)
+
+soup = BeautifulSoup(html, 'html.parser')
+book_name = soup.h1.text
+sections_url = get_sections_url(soup, 'list')
+
+with open('{}.txt'.format(book_name), 'w+') as f:
+    for section in sections_url:
+        section_url = urljoin(base_url, '{}/{}'.format(book_base_url, section))
+        html = get_content(section_url, header).replace('&nbsp', ' ')
+        soup = BeautifulSoup(html, 'html.parser')
+        section_name = soup.h1.text
+        content = '{}\n{}\n'.format(soup.h1.text, soup.find(id='content').text)
+        f.write(content)
